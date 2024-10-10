2ae86a1d5cb6594bc4af4c93159a6bb85a2b6cb4
Заплетнюк Діана <90466495+dizapl@users.noreply.github.com>
2024-10-10 19:34:05 +0300

    Create MainActivity2.kt

---
+++ Lab33/MainActivity2.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+import android.widget.Button
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+
+        val info = intent.getStringExtra("info")
+
+        val textViewInfo = findViewById<TextView>(R.id.textView2)
+        textViewInfo.text = info
+
+        val btnBack = findViewById<Button>(R.id.button4)
+        btnBack.setOnClickListener {
+            finish()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
